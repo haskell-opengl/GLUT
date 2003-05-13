1778bc51a11e07bbda6b5e7d26676ce6cb054e25
panne <unknown>
2003-05-13 16:50:53 +0000

    [project @ 2003-05-13 16:50:53 by panne] Fixed typo in comment

--- examples/Misc/FogCoordinate.hs
+++ examples/Misc/FogCoordinate.hs
@@ -1,5 +1,5 @@
 {-
-   FogCoordinat.hs (adapted from smooth.c which is (c) Silicon Graphics, Inc.)
+   FogCoordinate.hs (adapted from smooth.c which is (c) Silicon Graphics, Inc.)
    Copyright (c) Sven Panne 2003 <sven_panne@yahoo.com>
    This file is part of HOpenGL and distributed under a BSD-style license
    See the file libraries/GLUT/LICENSE
