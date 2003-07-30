d2cd41678548e5e1299bc833f4091cc059c7b8ae
wolfgang <unknown>
2003-07-30 09:22:42 +0000

    [project @ 2003-07-30 09:22:42 by wolfgang] OpenGL support for Mac OS X:
    Include the appropriate Apple-specific headers and link with
    Apple-specific "Frameworks"
    (unless we want to use X11 OpenGL libraries).

--- include/HsGLUT.h
+++ include/HsGLUT.h
@@ -13,7 +13,13 @@
 #ifndef HSGLUT_H
 #define HSGLUT_H
 
+#include "config.h"
+
+#ifdef USE_QUARTZ_OPENGL /* Mac OS X only */
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
+#endif
 
 extern void* hOpenGL_marshalBitmapFont(int fontID);
 extern void* hOpenGL_marshalStrokeFont(int fontID);
