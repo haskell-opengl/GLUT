4f17a6164fea8fc82ff7f2b73ab21b95dbf5fce1
panne <unknown>
2004-03-05 16:39:19 +0000

    [project @ 2004-03-05 16:39:19 by panne] Added more examples

---
+++ examples/RedBook/Double.hs
@@ -0,0 +1,69 @@
+{-
+   Double.hs (adapted from double.c which is (c) Silicon Graphics, Inc.)
+   Copyright (c) Sven Panne 2002-2004 <sven.panne@aedion.de>
+   This file is part of HOpenGL and distributed under a BSD-style license
+   See the file libraries/GLUT/LICENSE
+
+   This is a simple double buffered program.
+   Pressing the left mouse button rotates the rectangle.
+   Pressing the middle mouse button stops the rotation.
+-}
+
+import Data.IORef ( IORef, newIORef, readIORef, modifyIORef )
+import System.Exit ( exitWith, ExitCode(ExitSuccess) )
+import Graphics.UI.GLUT
+
+display :: IORef GLfloat -> DisplayCallback
+display spin = do
+   clear [ ColorBuffer ]
+   preservingMatrix $ do
+      s <- readIORef spin
+      rotate s (Vector3 0 0 1)
+      color (Color3 1 1 1 :: Color3 GLfloat)
+      rect (Vertex2 (-25) (-25)) (Vertex2 25 25 :: Vertex2 GLfloat)
+   swapBuffers
+
+spinDisplay :: IORef GLfloat -> IdleCallback
+spinDisplay spin = do
+   let wrap n s = if s > n then s - n else s
+   modifyIORef spin (wrap 360 . (+ 2))
+   postRedisplay Nothing
+
+myInit :: IO ()
+myInit = do
+   clearColor $= Color4 0 0 0 0
+   shadeModel $= Flat
+
+reshape :: ReshapeCallback
+reshape size = do
+   viewport $= (Position 0 0, size)
+   matrixMode $= Projection
+   loadIdentity
+   ortho (-50) 50 (-50) 50 (-1) 1
+   matrixMode $= Modelview 0
+   loadIdentity
+
+keyboardMouse :: IORef GLfloat -> KeyboardMouseCallback
+keyboardMouse spin (MouseButton b) Down _ _ =
+   idleCallback $= case b of
+      LeftButton -> Just (spinDisplay spin)
+      _ -> Nothing
+-- ESC not handled in the original example, but useful nevertheless
+keyboardMouse _ (Char '\27') Down _ _ = exitWith ExitSuccess
+keyboardMouse _ _ _ _ _ = return ()
+
+--  Request double buffer display mode.
+--  Register mouse input callback functions
+main :: IO ()
+main = do
+   (progName, _args) <- getArgsAndInitialize
+   initialDisplayMode $= [ DoubleBuffered, RGBMode ]
+   initialWindowSize $= Size 250 250
+   initialWindowPosition $= Position 100 100
+   createWindow progName
+   myInit
+   spin <- newIORef 0
+   displayCallback $= display spin
+   reshapeCallback $= Just reshape
+   keyboardMouseCallback $= Just (keyboardMouse spin)
+   mainLoop
---
+++ examples/RedBook/Lines.hs
@@ -0,0 +1,88 @@
+{-
+   Lines.hs (adapted from lines.c which is (c) Silicon Graphics, Inc.)
+   Copyright (c) Sven Panne 2002-2004 <sven.panne@aedion.de>
+   This file is part of HOpenGL and distributed under a BSD-style license
+   See the file libraries/GLUT/LICENSE
+
+   This program demonstrates geometric primitives and their attributes.
+-}
+
+import System.Exit ( exitWith, ExitCode(ExitSuccess) )
+import Graphics.UI.GLUT
+
+drawOneLine :: Vertex2 GLfloat -> Vertex2 GLfloat -> IO ()
+drawOneLine p1 p2 = renderPrimitive Lines $ do vertex p1; vertex p2
+
+myInit :: IO ()
+myInit = do
+   clearColor $= Color4 0 0 0 0
+   shadeModel $= Flat
+
+display :: DisplayCallback
+display = do
+   clear [ ColorBuffer ]
+   -- select white for all lines
+   color (Color3 1.0 1.0 1.0 :: Color3 GLfloat)
+
+   -- in 1st row, 3 lines, each with a different stipple
+   lineStipple $= Just (1, 0x0101)  --  dotted
+   drawOneLine (Vertex2  50 125) (Vertex2 150 125)
+   lineStipple $= Just (1, 0x00FF)  --  dashed
+   drawOneLine (Vertex2 150 125) (Vertex2  50 125)
+   lineStipple $= Just (1, 0x1C47)  --  dash/dot/dash
+   drawOneLine (Vertex2 250 125) (Vertex2 350 125)
+
+   -- in 2nd row, 3 wide lines, each with different stipple
+   lineWidth $= 5.0
+   lineStipple $= Just (1, 0x0101)  --  dotted
+   drawOneLine (Vertex2  50 100) (Vertex2 150 100)
+   lineStipple $= Just (1, 0x00FF)  --  dashed
+   drawOneLine (Vertex2 150 100) (Vertex2 250 100)
+   lineStipple $= Just (1, 0x1C47)  --  dash/dot/dash
+   drawOneLine (Vertex2 250 100) (Vertex2 350 100)
+   lineWidth $= 1.0
+
+   -- in 3rd row, 6 lines, with dash/dot/dash stipple
+   -- as part of a single connected line strip
+   lineStipple $= Just (1, 0x1C47)  --  dash/dot/dash
+   renderPrimitive LineStrip $ mapM_ vertex [ Vertex2 (50+(i*50)) (75 :: GLint) | i <- [0..6] ]
+
+   -- in 4th row, 6 independent lines with same stipple
+   sequence_ [ drawOneLine (Vertex2 (50+( i   *50)) 50)
+                           (Vertex2 (50+((i+1)*50)) 50) | i <- [0..5] ]
+
+   -- in 5th row, 1 line, with dash/dot/dash stipple
+   -- and a stipple repeat factor of 5
+   lineStipple $= Just (5, 0x1C47)  --  dash/dot/dash
+   drawOneLine (Vertex2 50 25) (Vertex2 350 25)
+
+   lineStipple $= Nothing
+   flush
+
+reshape :: ReshapeCallback
+reshape size@(Size w h) = do
+   viewport $= (Position 0 0, size)
+   matrixMode $= Projection
+   loadIdentity
+   ortho2D 0 (fromIntegral w) 0 (fromIntegral h)
+   -- the following line is not in the original example, but it's good style...
+   matrixMode $= Modelview 0
+
+keyboard :: KeyboardMouseCallback
+keyboard (Char '\27') Down _ _ = exitWith ExitSuccess
+keyboard _ _ _ _ = return ()
+
+--  Request double buffer display mode.
+--  Register mouse input callback functions
+main :: IO ()
+main = do
+   (progName, _args) <- getArgsAndInitialize
+   initialDisplayMode $= [ SingleBuffered, RGBMode ]
+   initialWindowSize $= Size 400 150
+   initialWindowPosition $= Position 100 100
+   createWindow progName
+   myInit
+   displayCallback $= display
+   reshapeCallback $= Just reshape
+   keyboardMouseCallback $= Just keyboard
+   mainLoop
