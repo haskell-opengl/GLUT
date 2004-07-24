2ecde34a4a221c101af992c4c5184431eb26b22e
panne <unknown>
2004-07-24 11:48:05 +0000

    [project @ 2004-07-24 11:48:05 by panne] Added a few more examples from chapter 6 of the Red Book (Blending, Antialiasing, Fog, and Polygon Offset)

---
+++ examples/RedBook/Alpha.hs
@@ -0,0 +1,91 @@
+{-
+   Alpha.hs (adapted from alpha.c which is (c) Silicon Graphics, Inc.)
+   Copyright (c) Sven Panne 2002-2004 <sven.panne@aedion.de>
+   This file is part of HOpenGL and distributed under a BSD-style license
+   See the file libraries/GLUT/LICENSE
+
+   This program draws several overlapping filled polygons to demonstrate the
+   effect order has on alpha blending results. Use the 't' key to toggle the
+   order of drawing polygons.
+-}
+
+import Data.Char ( toLower )
+import Data.IORef ( IORef, newIORef )
+import System.Exit ( exitWith, ExitCode(ExitSuccess) )
+import Graphics.UI.GLUT
+
+data State = State { leftFirst :: IORef Bool }
+
+makeState :: IO State
+makeState = do
+   l <- newIORef True
+   return $ State { leftFirst = l }
+
+-- Initialize alpha blending function.
+myInit :: IO ()
+myInit = do
+   blendEquation $= Just FuncAdd
+   blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
+   shadeModel $= Flat
+   clearColor $= Color4 0 0 0 0
+
+drawLeftTriangle :: IO ()
+drawLeftTriangle =
+   -- draw yellow triangle on LHS of screen
+   renderPrimitive Triangles $ do
+      color (Color4 1 1 0 (0.75 :: GLfloat))
+      vertex (Vertex3 0.1 0.9 (0 :: GLfloat))
+      vertex (Vertex3 0.1 0.1 (0 :: GLfloat))
+      vertex (Vertex3 0.7 0.5 (0 :: GLfloat))
+
+drawRightTriangle :: IO ()
+drawRightTriangle =
+   -- draw cyan triangle on RHS of screen
+   renderPrimitive Triangles $ do
+      color (Color4 0 1 1 (0.75 :: GLfloat))
+      vertex (Vertex3 0.9 0.9 (0 :: GLfloat))
+      vertex (Vertex3 0.3 0.5 (0 :: GLfloat))
+      vertex (Vertex3 0.9 0.1 (0 :: GLfloat))
+
+display :: State -> DisplayCallback
+display state = do
+   clear [ ColorBuffer ]
+   l <- get (leftFirst state)
+   if l
+      then do drawLeftTriangle; drawRightTriangle
+      else do drawRightTriangle; drawLeftTriangle
+   flush
+
+reshape :: ReshapeCallback
+reshape size@(Size w h) = do
+   viewport $= (Position 0 0, size)
+   matrixMode $= Projection
+   loadIdentity
+   let wf = fromIntegral w
+       hf = fromIntegral h
+   if w <= h
+      then ortho2D 0 1 0 (hf/wf)
+      else ortho2D 0 (wf/hf) 0 1
+
+keyboard :: State -> KeyboardMouseCallback
+keyboard state (Char c) Down _ _ = case toLower c of
+   't'   -> do leftFirst state $~ not; postRedisplay Nothing
+   '\27' -> exitWith ExitSuccess   -- Escape key
+   _     -> return ()
+keyboard _ _ _ _ _ = return ()
+
+-- Main Loop
+-- Open window with initial window size, title bar, RGBA display mode, and
+-- handle input events.
+main :: IO ()
+main = do
+   (progName, _args) <- getArgsAndInitialize
+   initialDisplayMode $= [ SingleBuffered, RGBMode ]
+   initialWindowSize $= Size 200 200
+   createWindow progName
+   state <- makeState
+   myInit
+   reshapeCallback $= Just reshape
+   keyboardMouseCallback $= Just (keyboard state)
+   displayCallback $= display state
+   mainLoop
---
+++ examples/RedBook/BlendEqn.hs
@@ -0,0 +1,95 @@
+{-
+   BlendEqn.hs (adapted from blendeqn.c which is (c) Silicon Graphics, Inc.)
+   Copyright (c) Sven Panne 2002-2004 <sven.panne@aedion.de>
+   This file is part of HOpenGL and distributed under a BSD-style license
+   See the file libraries/GLUT/LICENSE
+
+   Demonstrate the different blending functions available with the OpenGL
+   imaging subset. This program demonstrates use of blendEquation.
+
+   The following keys change the selected blend equation function:
+ 
+       'a'  ->  FuncAdd
+       's'  ->  FuncSubtract
+       'r'  ->  FuncReverseSubtract
+       'm'  ->  Min
+       'x'  ->  Max
+-}
+
+import Data.Char ( toLower )
+import System.Exit ( exitWith, ExitCode(ExitSuccess) )
+import Graphics.UI.GLUT
+
+myInit :: IO ()
+myInit = do
+   clearColor $= Color4 1 1 0 0
+   blendFunc $= (One, One)
+   blendEquation $= Just FuncAdd
+
+display :: DisplayCallback
+display = do
+   clear [ ColorBuffer ]
+
+   color (Color3 0 0 (1 :: GLfloat))
+   rect (Vertex2 (-0.5) (-0.5)) (Vertex2 0.5 (0.5 :: GLfloat))
+
+   flush
+
+reshape :: ReshapeCallback
+reshape size@(Size w h) = do
+   let aspect = fromIntegral w / fromIntegral h
+
+   viewport $= (Position 0 0, size)
+   matrixMode $= Projection
+   loadIdentity
+   if aspect < 1
+      then let aspect' = recip aspect
+           in ortho (-aspect') aspect' (-1) 1 (-1) 1
+      else ortho (-1) 1 (-aspect) aspect (-1) 1
+   matrixMode $= Modelview 0
+
+keyboard :: KeyboardMouseCallback
+keyboard (Char c) Down _ _ = case toLower c of
+   -- Colors are added as: (0, 0, 1) + (1, 1, 0) = (1, 1, 1)
+   -- which will produce a white square on a yellow background.
+   'a'   -> setBlendEquation FuncAdd
+
+   -- Colors are subtracted as: (0, 0, 1) - (1, 1, 0) = (-1, -1, 1)
+   -- which is clamped to (0, 0, 1), producing a blue square on a
+   -- yellow background
+   's'   -> setBlendEquation FuncSubtract
+
+   -- Colors are subtracted as: (1, 1, 0) - (0, 0, 1) = (1, 1, -1)
+   -- which is clamed to (1, 1, 0). This produces yellow for both
+   -- the square and the background.
+   'r'   -> setBlendEquation FuncReverseSubtract
+
+   -- The minimum of each component is computed, as
+   -- [min(0, 1), min(0, 1), min(1, 0)] which equates to (0, 0, 0).
+   -- This will produce a black square on the yellow background.
+   'm'   -> setBlendEquation Min
+
+   -- The minimum of each component is computed, as
+   -- [max(0, 1), max(0, 1), max(1, 0)] which equates to (1, 1, 1)
+   -- This will produce a white square on the yellow background.
+   'x'   -> setBlendEquation Max
+
+   '\27' -> exitWith ExitSuccess
+   _     -> return ()
+   where setBlendEquation e = do
+            blendEquation $= Just e
+            postRedisplay Nothing
+keyboard _ _ _ _ = return ()
+
+main :: IO ()
+main = do
+   (progName, _args) <- getArgsAndInitialize
+   initialDisplayMode $= [ SingleBuffered, RGBMode ]
+   initialWindowSize $= Size 512 512
+   initialWindowPosition $= Position 100 100
+   createWindow progName
+   myInit
+   reshapeCallback $= Just reshape
+   keyboardMouseCallback $= Just keyboard
+   displayCallback $= display
+   mainLoop
