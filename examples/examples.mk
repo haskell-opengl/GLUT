b8560a61101bf669e76650919c27077818b5d1c0
Sven Panne <svenpanne@gmail.com>
2013-08-14 11:24:51 +0200

    Added some dirty but useful Makefile machinery for building the examples. Still a hack, doesn't really track dependencies.

---
+++ examples/examples.mk
@@ -0,0 +1,25 @@
+ALL_DIRS         := $(SUBDIRS:%=all-%)
+CLEAN_DIRS       := $(SUBDIRS:%=clean-%)
+HC               := ghc
+HC_FLAGS         := -Wall
+HS_SOURCES       := $(wildcard *.hs)
+HS_PROG_SOURCES  := $(filter-out $(EXCLUDED_SOURCES), $(HS_SOURCES))
+HS_PROGS         := $(HS_PROG_SOURCES:.hs=)
+
+.PHONY: all $(ALL_DIRS) clean $(CLEAN_DIRS)
+
+all: $(ALL_DIRS) $(HS_PROGS)
+
+$(ALL_DIRS): 
+	$(MAKE) -C $(@:all-%=%) all
+
+clean: $(CLEAN_DIRS)
+ifneq ($(HS_SOURCES),)
+	$(RM) $(HS_SOURCES:.hs=.hi) $(HS_SOURCES:.hs=.o) $(HS_PROGS)
+endif
+
+$(CLEAN_DIRS): 
+	$(MAKE) -C $(@:clean-%=%) clean
+
+$(HS_PROGS): %: %.hs
+	$(HC) --make $(HC_FLAGS) $<
