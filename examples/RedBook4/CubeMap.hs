8936783bda070cfb5c9fd6ea69323398b900773e
Sven Panne <svenpanne@gmail.com>
2013-09-12 16:18:01 +0200

    Adapted code after recent texture target changes.

--- examples/RedBook4/CubeMap.hs
+++ examples/RedBook4/CubeMap.hs
@@ -36,10 +36,10 @@ withCheckImage (TextureSize2D w h) n f act =
                      | otherwise              = 255 ] $
    act. PixelData RGBA UnsignedByte
 
-makeImage :: CubeMapTarget -> (GLubyte -> (Color4 GLubyte)) -> IO ()
+makeImage :: TextureTargetCubeMapFace -> (GLubyte -> (Color4 GLubyte)) -> IO ()
 makeImage target f =
    withCheckImage imageSize 0x1 f $
-      texImage2D (TextureCubeMap target) NoProxy 0  RGBA' imageSize 0
+      texImage2D target NoProxy 0  RGBA' imageSize 0
 
 myInit :: IO ()
 myInit = do
@@ -48,10 +48,10 @@ myInit = do
    shadeModel $= Smooth
 
    rowAlignment Unpack $= 1
-   textureWrapMode (TextureCubeMap undefined) S $= (Repeated, Repeat)  -- TODO: WRONG!!!
-   textureWrapMode (TextureCubeMap undefined) T $= (Repeated, Repeat)  -- TODO: WRONG!!!
-   textureWrapMode (TextureCubeMap undefined) R $= (Repeated, Repeat)  -- TODO: WRONG!!!
-   textureFilter (TextureCubeMap undefined) $= ((Nearest, Nothing), Nearest)  -- TODO: WRONG!!!
+   textureWrapMode TextureCubeMap S $= (Repeated, Repeat)
+   textureWrapMode TextureCubeMap T $= (Repeated, Repeat)
+   textureWrapMode TextureCubeMap R $= (Repeated, Repeat)
+   textureFilter TextureCubeMap $= ((Nearest, Nothing), Nearest)
 
    makeImage TextureCubeMapPositiveX (\c -> Color4   c c   c 255)
    makeImage TextureCubeMapNegativeX (\c -> Color4   0 c   c 255)
@@ -66,7 +66,7 @@ myInit = do
 
    textureFunction $= Modulate
 
-   texture (TextureCubeMap undefined) $= Enabled  -- TODO: WRONG!!!
+   texture TextureCubeMap $= Enabled
    lighting $= Enabled
    light (Light 0) $= Enabled
    autoNormal $= Enabled
